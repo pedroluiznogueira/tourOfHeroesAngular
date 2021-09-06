f542c7a330756b3ffc4f173b99532c2bf2b7aa2c
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-09-06 18:42:57 -0300

    Setting up a In Memory Web API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './services/in-memory-data.service';
+
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { HerodetailsComponent } from './components/herodetails/herodetails.component';
 import { MessagesComponent } from './components/messages/messages.component';
-import { RouterModule } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
@@ -22,6 +25,10 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }      
+    ),
     RouterModule.forRoot([
       { path: 'heroes', component: HeroesComponent },
       { path: 'dashboard', component: DashboardComponent },
