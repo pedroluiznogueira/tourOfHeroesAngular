3910e9459a4595744a654e0825d8004cf7708dd3
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-09-06 19:04:12 -0300

    Requisitando o hero por id

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -38,18 +38,22 @@ export class HeroService {
     private messageService: MessageService,
     private http: HttpClient) { }
 
-    getHeroes(): Observable<Hero[]> {
-      return this.http.get<Hero[]>(this.heroesUrl)
-        .pipe(
-          catchError(this.handleError<Hero[]>('getHeroes', []))
-        );
-    }
+  /** GET heroes from the server */
+  getHeroes(): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        tap(_ => this.log('fetched heroes')),
+        catchError(this.handleError<Hero[]>('getHeroes', []))
+      );
+  }
 
-  getHero(heroIdFromRoute: number): Observable<Hero> {
-    const hero = HEROES.find(hero => hero.id === heroIdFromRoute)!;
-    this.messageService.add(`The hero with id ${heroIdFromRoute} was fetched`);
-    
-    return of(hero);
+  /** GET hero by id. Will 404 if id not found */
+  getHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
   }
 
   /** Log a HeroService message with the MessageService */
