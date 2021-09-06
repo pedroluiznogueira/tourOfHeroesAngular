2ac32b98eeaf7a273155ac6c8a8009e99e4274a0
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-09-06 18:48:06 -0300

    Setting up o hero service para usar a API que faz o request do in memory message service

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -3,14 +3,19 @@ import { Hero } from '../models/Hero';
 import { HEROES } from '../models/mockHeroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
+  private heroesUrl = 'api/heroes';  // URL to web api
+
   // injetando o message service
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private messageService: MessageService,
+    private httpClient: HttpClient) { }
 
   // adaptando a função para que retorne um Observable
   getHeroes(): Observable<Hero[]> {
@@ -24,5 +29,10 @@ export class HeroService {
     this.messageService.add(`The hero with id ${heroIdFromRoute} was fetched`);
     
     return of(hero);
+  }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 }
